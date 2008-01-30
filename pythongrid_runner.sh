5a3b8b3ed99cbb0c66d6bd97df09fc2f1d943faa
ong <ong@tuebingen.mpg.de>
2008-01-30 16:45:05 +0000

    small change
    git-svn-id: https://svn.tuebingen.mpg.de:/ag-raetsch/tools/python/pythongrid@7647 e1793c9e-67f9-0310-80fc-b846ff1f7b36

--- pythongrid_runner.sh
+++ pythongrid_runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "setting env"
-export PYTHONPATH=~/lib/python2.5/site-packages/:~/svn/tools/python/:~/svn/projects/shogun_regtrick/python-modular/examples
+export PYTHONPATH=~/lib/python2.5/site-packages/:~/svn/tools/python/:~/svn/tools/python/pythongrid:$PYTHONPATH
 export LD_LIBRARY_PATH=/usr/local/sge/lib/lx26-amd64/
 
 ~/svn/tools/python/pythongrid/pythongrid.py $1
