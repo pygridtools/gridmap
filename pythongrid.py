59ae840eb29f482e9164c11288de109ae478fb01
cwidmer <cwidmer@tuebingen.mpg.de>
2008-03-10 16:40:22 +0000

    added pydoc
    git-svn-id: https://svn.tuebingen.mpg.de:/ag-raetsch/tools/python/pythongrid@8112 e1793c9e-67f9-0310-80fc-b846ff1f7b36

--- pythongrid.py
+++ pythongrid.py
@@ -11,16 +11,36 @@ import thread
 
 
 
-class Job:
+class Job (object):
+  '''
+  Central entity that wrappes a function and its data. Basically,
+  a job consists of a function, its argument list, its
+  keyword list and a field "ret" which is filled, when
+  the execute method gets called
+  '''
 
   f=None
   args=()
   kwlist={}
   ret=None
 
+  nativeResources=""
 
-  def __init__(self, f, args, kwlist={}, additionalpath=[]):
- 
+
+  def __init__(self, f, args, kwlist={}, additionalpaths=[]):
+    '''
+    constructor of Job
+
+    @param f: a function, which should be executed.
+    @type f: function
+    @param args: argument list of function f
+    @type args: list
+    @param kwlist: dictionary of keyword arguments
+    @type kwlist: dict
+    @param additionalpaths: additional paths for use on cluster nodes
+    @type additionalpaths: list of strings
+    '''
+
     self.f=f
     self.args=args
     self.kwlist=kwlist
@@ -29,15 +49,101 @@ class Job:
     self.pythonpath=sys.path
     self.pythonpath.append(os.getcwd())
 
+
     #TODO: set additional path from a config file
-    self.pythonpath.extend(additionalpath)
+    #self.pythonpath.extend(additionalpaths)
+
 
   def execute(self):
+    '''
+    Executes function f with given arguments and writes return value to field ret.
+    Input data is removed after execution to save space.
+    '''
     self.ret=apply(self.f,self.args,self.kwlist)
 
+    #TODO make this optional!
+    #remove input data
+    self.args=[]
+    self.kwlist={}
 
-#TODO derive this from Job, unify!
+
+
+class KybJob(Job):
+  '''
+  Specialization of generic Job that provides an interface to Kyb-specific options.
+  Not quite finished yet, interface will most likely change in the near future. (march 2008)
+  '''
+
+  h_vmem=""
+  arch=""
+  tmpfree=""
+  h_cpu=""
+  h_rt=""
+  express=""
+  matlab=""
+  simulink=""
+  compiler=""
+  imagetb=""
+  opttb=""
+  stattb=""
+  sigtb=""
+  cplex=""
+  nicetohave=""
+
+  #http://www.python.org/download/releases/2.2.3/descrintro/#property
+
+  def getNativeResources(self):
+    '''
+    define python-style getter
+    '''
+
+    ret=""
+    #TODO
+    value="TODO"
+
+    if (value!=""):
+      ret=ret + " -l " + "h_vmem" + "=" + self.h_vmem
+
+    return ret
+
+  def setNativeResources(self, x):
+    '''
+    define python-style setter
+    @param x: nativeResources string to be set
+    @type x: string
+    '''
+
+    self.__nativeResources=x
+
+  nativeResources=property(getNativeResources, setNativeResources)
+
+
+#  def __getattribute__(self, name):
+#
+#    if (name == "nativeResources"):
+#
+#      ret=""
+#
+#      for attr in self.__dict__.keys():
+#
+#        print attr
+#
+#        value = self.__dict__[attr]
+#        if (value!=""):
+#          ret=ret + " -l " + attr + "=" + value
+#
+#      return ret
+#
+#
+#    else:
+#
+#      return Job.__getattribute__(self, name)
+#
+
+#TODO this class will most likely disappear, soon.
 class MethodJob:
+
+  #TODO derive this from Job, unify!
 
   methodName=""
   obj=None
@@ -46,7 +152,16 @@ class MethodJob:
   ret=None
 
   def __init__(self, m, args, kwlist={}):
- 
+    '''
+
+    @param m: method to execute
+    @type m: method
+    @param args: list of arguments
+    @type args: list
+    @param kwlist: keyword list
+    @type kwlist: dict
+    '''
+
     self.methodName=m.im_func.func_name
     self.obj=m.im_self
     self.args=args
@@ -59,23 +174,41 @@ class MethodJob:
 
 
 def createFileName():
+  '''
+  Generates random filename.
+  '''
   return "pickle_" + randomString(10) + ".bz2"
 
 
 
 def randomString(length):
+  '''
+  Generates random string of specified length. Only a 4-letter alphabet is used atm.
+  @param length: length of random string
+  @type length: integer
+  '''
   alphabet=['a','b','c','d']
 
   string = [random.choice(alphabet) for j in range(length)]
   string = "".join(string)
+
+
 
   return string
 
 
 
 def processJobsLocally(jobs, numThreads=1):
-  """Run jobs on local machine in a multithreaded manner"""
-  
+  '''
+  Run jobs on local machine in a multithreaded manner, providing the same interface.
+  NOT finished yet.
+
+  @param jobs: list of jobs to be executed locally.
+  @type jobs: list of Job objects
+  @param numThreads: defines how many threads should be used to execute the jobs.
+  @type numThreads: integer
+  '''
+
   for job in jobs:
 
 
@@ -84,11 +217,13 @@ def processJobsLocally(jobs, numThreads=1):
 
 
 def processJobs(jobs):
-  """Submit an array job.
-  Note, need file called sleeper.sh in home directory. An example:
-  echo 'Hello World $1'
-  """
+  '''
+  Method used to send a list of jobs onto the cluster. Method will wait for all jobs to finish and return
+  the list of jobs with the respective ret field set.
 
+  @param jobs: list of jobs to be executed
+  @type jobs: list of Job objects
+  '''
 
   dir=os.path.expanduser("~/")
 
@@ -128,14 +263,13 @@ def processJobs(jobs):
     except Exception, detail:
       print "error while pickling file: " + dir + fileName
       print detail
-      
-      
+
+
 
     print 'Creating job template'
     jt = s.createJobTemplate()
 
     #TODO FILENAME!
-
     command=os.path.expanduser('~/svn/tools/python/pythongrid/pythongrid_runner.sh')
 
     jt.remoteCommand = command
@@ -150,24 +284,24 @@ def processJobs(jobs):
 
     joblist.append(jobid)
 
-    #To clean things up, we delete the job template. This frees the memory DRMAA 
+    #To clean things up, we delete the job template. This frees the memory DRMAA
     #set aside for the job template, but has no effect on submitted jobs.
     #s.deleteJobTemplate(jt)
-  
 
-  #wait for jobs to finish 
+
+  #wait for jobs to finish
   s.synchronize(joblist, DRMAA.Session.TIMEOUT_WAIT_FOREVER, True)
 
   print "success: all jobs finished"
 
   s.exit()
 
-  retJobs=[]  
+  retJobs=[]
 
 
   #collect results
   for fileName in fileNames:
- 
+
     path = fileName + ".out"
 
     print path
@@ -179,21 +313,26 @@ def processJobs(jobs):
 
   #clean up path file
   os.remove(path_file)
-  
-  return retJobs
-    
 
+  return retJobs
 
 
 ################################################################
 #      The following code will be executed on the cluster      #
 ################################################################
 
-
 def runJob(pickleFileName, path_file):
-  """Runs job which was pickled to a file called pickledFileName
-  """
- 
+  '''
+  Runs job which was pickled to a file called pickledFileName.
+  Saved paths are loaded in advance from another Pickleded object
+  called path_file.
+
+  @param pickleFileName: filename of pickled Job object
+  @type pickleFileName: string
+  @param path_file: filename of pickled list of strings
+  @type path_file: string
+  '''
+
   dir=""
 
   #restore pythonpath on cluster node
@@ -206,22 +345,37 @@ def runJob(pickleFileName, path_file):
 
   job.execute()
 
+  #remove input file
+  os.remove(dir + pickleFileName)
+
   outPath = dir + pickleFileName + ".out"
-  
+
   io_pickle.save(outPath, job)
 
-  #TODO clean up files
-  
 
 class Usage(Exception):
+  '''
+  Simple Exception for cmd-line user-interface.
+  '''
 
   def __init__(self, msg):
+    '''
+    Constructor of simple Exception.
+    @param msg: exception message
+    @type msg: string
+    '''
 
     self.msg = msg
 
-  
+
 
 def main(argv=None):
+  '''
+  Generic main
+
+  @param argv: list of arguments
+  @type argv: list of strings
+  '''
 
 
   if argv is None:
@@ -245,7 +399,7 @@ def main(argv=None):
 
     return 2
 
-  
+
 
 if __name__ == "__main__":
 
