a619b8d64c48e162d38b86c22ba2101c981c4331
cwidmer <cwidmer@tuebingen.mpg.de>
2008-06-12 10:53:10 +0000

    adjusted to new interface, added example for submit&forget
    git-svn-id: https://svn.tuebingen.mpg.de:/ag-raetsch/tools/python/pythongrid@9577 e1793c9e-67f9-0310-80fc-b846ff1f7b36

--- example.py
+++ example.py
@@ -1,54 +1,65 @@
 import sys
 import getopt
-from pythongrid import Job, KybJob, MethodJob, processJobs, Usage
+from pythongrid import KybJob, MethodJob, processJobs, Usage, submitJobs, collectJobs, getStatus
+import time
 
-
-#NOTE: !!this is necessary in order to be able to use the full function identifier!!
+#needs to be imported, such that module name can be referred to explicitly
 import example
-
-
-def makeMethodJobs():
-  '''
-  Creates a list of Method Jobs.
-  '''
-
-  myobj=Test()
-
-  jobs=[]
-  jobs.append(MethodJob(myobj.doSomething, ["aaaaaXXXXXXXXXXX"]))
-  jobs.append(MethodJob(myobj.doSomething, ["bbbbbbbbbXXXXXXXXXYa"]))
-  jobs.append(MethodJob(myobj.doSomething, ["ccccccccccXXXXXXXXX"]))
-
-  return jobs
 
 
 def makeJobs():
   '''
-  Creates a list of regular (function) Jobs. One needs to use the full identifier
-  such that the module name is saved as well.
+  Creates a list of Jobs.   
   '''
 
   jobs=[]
-  j1 = KybJob(example.testFunction, ["aaaaaXXXXXXXXXXX"])
+  j1 = KybJob(example.computeFaculty, [20])
   j1.h_vmem="300M"
-
-  print "job #1: ", j1.nativeSpecification
-
 
   jobs.append(j1)
 
-  jobs.append(Job(example.testFunction, ["bbbbbbbbbXXXXXXXXXYa"]))
-  jobs.append(Job(example.testFunction, ["ccccccccccXXXXXXXXX"]))
-  jobs.append(Job(example.testFunction, ["bbbbbbbbbXXXXXXXXXYa"]))
-  jobs.append(Job(example.testFunction, ["ccccccccccXXXXXXXXX"]))
+  #One needs to use the full identifier such that the module name is explicit.
+  jobs.append(KybJob(example.computeFaculty, [30]))
+  jobs.append(KybJob(example.computeFaculty, [40]))
+  jobs.append(KybJob(example.computeFaculty, [50]))
+  jobs.append(KybJob(example.computeFaculty, [50]))
 
   return jobs
 
 
 def runExample():
-
+  
   print "====================================="
+  print "======= Local Multithreading ========"
+  print "====================================="
+  print ""
+  print ""
+
   print "generating fuction jobs"
+
+  functionJobs = makeJobs()
+
+  print "output ret field in each job before multithreaded computation"
+  for (i, job) in enumerate(functionJobs):
+    print "Job with id: ", i, "- ret: ", job.ret
+
+  print ""
+  print "sending function jobs to local machine"
+  print ""
+
+  processedFunctionJobs = processJobs(functionJobs, local=2)
+
+  print "ret fields AFTER execution on local machine"
+  for (i, job) in enumerate(processedFunctionJobs):
+    print "Job with id: ", i, "- ret: ", job.ret
+
+  print ""
+  print ""
+  print "====================================="
+  print "=======   Submit and Wait    ========"
+  print "====================================="
+  print ""
+  print ""
 
   functionJobs = makeJobs()
 
@@ -69,69 +80,43 @@ def runExample():
 
   print ""
   print ""
-
   print "====================================="
-  print "======= Local Multithreading ========"
+  print "=======  Submit and Forget   ========"
   print "====================================="
-
-  print "generating fuction jobs"
-
-  functionJobs = makeJobs()
-
-  print "output ret field in each job before multithreaded computation"
-  for (i, job) in enumerate(functionJobs):
-    print "Job with id: ", i, "- ret: ", job.ret
-
   print ""
-  print "sending function jobs to local machine"
   print ""
 
-  processedFunctionJobs = processJobs(functionJobs, locally=2)
 
-  print "ret fields AFTER execution on local machine"
-  for (i, job) in enumerate(processedFunctionJobs):
+  print 'demo session'
+  myjobs = makeJobs()
+
+  (sid,jobids)=submitJobs(myjobs)
+
+  print 'checking whether finished'
+  while not getStatus(sid,jobids):
+    time.sleep(2)
+        
+  print 'collecting jobs'
+  retjobs=collectJobs(sid,jobids,myjobs)
+  print "ret fields AFTER execution on cluster"
+  for (i, job) in enumerate(retjobs):
     print "Job with id: ", i, "- ret: ", job.ret
 
-
-  print "====================================="
- #print "generating method jobs"
-
-  #methodJobs = makeMethodJobs()
-
-  #print methodJobs[0].obj.sideEffect
-  #print methodJobs[0].ret
-
-  #processedMethodJobs = processJobs(methodJobs)
-
-  #print processedMethodJobs[0].obj.sideEffect
-  #print processedMethodJobs[0].ret
+  print '--------------'
 
 
-def testFunction(string):
+
+def computeFaculty(n):
   '''
   Dummy function for testing a regular (function) Job.
   '''
-  print string
 
-  for i in xrange(1000):
-    i+1
+  ret=1
 
-  return string+string;
+  for i in xrange(n):
+    ret=ret*(i+1)
 
-
-
-class Test:
-  '''
-  Dummy class for testing MethodJob. Might now work ATM, will be fixed, soon.
-  '''
-
-  sideEffect="not set"
-
-  def doSomething(self, a):
-    print "executing test"
-    self.sideEffect="sideeffect set"
-
-    return a+a
+  return ret
 
 
 
