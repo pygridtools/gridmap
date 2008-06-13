e804333e12ce31025fe9b1f1e312d3ebdf043e7e
ong <ong@tuebingen.mpg.de>
2008-06-13 10:11:58 +0000

    howto for shogun
    git-svn-id: https://svn.tuebingen.mpg.de:/ag-raetsch/tools/python/pythongrid@9599 e1793c9e-67f9-0310-80fc-b846ff1f7b36

--- tarme.sh
+++ tarme.sh
@@ -2,5 +2,5 @@
 cd ../
 tar cvzf pythongrid/pythongrid-0.1.tar.gz \
 pythongrid/C_elegans_acc_gc.csv pythongrid/easysvm_example.py pythongrid/example.py \
-pythongrid/pythongrid.py pythongrid/README pythongrid/TODO
+pythongrid/pythongrid.py pythongrid/README pythongrid/TODO pythongrid/installing-easysvm.txt
 cd pythongrid
