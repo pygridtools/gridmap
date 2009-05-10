d2cf35f8dab5387509f2a55d445c255d4e0787e1
ong <ong@tuebingen.mpg.de>
2009-05-10 12:32:13 +0000

    Added a small test that the C DRMAA library is working
    git-svn-id: https://svn.tuebingen.mpg.de:/ag-raetsch/tools/python/pythongrid@13855 e1793c9e-67f9-0310-80fc-b846ff1f7b36

---
+++ test/test_drmaa.c
@@ -0,0 +1,24 @@
+/* Some simple code just to check that installation is working */
+
+#include <drmaa.h>
+
+int main (int argc, char **argv) {
+  char error[DRMAA_ERROR_STRING_BUFFER];
+  int errnum = 0;
+  
+  errnum = drmaa_init (NULL, error, DRMAA_ERROR_STRING_BUFFER);
+  
+  if (errnum != DRMAA_ERRNO_SUCCESS) {
+    fprintf (stderr, "Could not initialize the DRMAA library: %s\n", error);
+    return 1;
+  }
+  printf ("DRMAA library was started successfully\n");
+  
+  errnum = drmaa_exit (error, DRMAA_ERROR_STRING_BUFFER);
+  
+  if (errnum != DRMAA_ERRNO_SUCCESS) {
+    fprintf (stderr, "Could not shut down the DRMAA library: %s\n", error);
+    return 1;
+  }
+  return 0;
+}
