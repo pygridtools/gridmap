3768ba9392b2ea085d5b44405519f3ce416f9666
Christian Widmer <cwidmer@tuebingen.mpg.de>
2012-07-25 04:10:24 +0200

    deleted old examples (will revive some of them later) and cleaned up zmq examples

--- tarme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ../
-tar cvzf pythongrid/pythongrid-0.2.tar.gz \
-pythongrid/C_elegans_acc_gc.csv pythongrid/easysvm_example.py pythongrid/example.py \
-pythongrid/pythongrid.py pythongrid/README pythongrid/TODO pythongrid/installing-easysvm.txt
-cd pythongrid
