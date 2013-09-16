44a42eb89edcad0ac180d196ceef41b6bc45b606
Dan Blanchard <dblanchard@ets.org>
2013-09-15 20:45:10 -0400

    Add workaround for Python 3 compatability bug in drmaa-python

--- gridmap/job.py
+++ gridmap/job.py
@@ -278,7 +278,16 @@ def _append_job_to_session(session, job, uniq_id, job_num, temp_dir='/scratch/',
     jt = session.createJobTemplate()
 
     # fetch env vars from shell
-    jt.jobEnvironment = os.environ
+    env = os.environ
+    # Work around for bug in drmaa-python
+    if sys.version_info >= (3, 0:
+        for env_var, value in os.environ.items():
+            if isinstance(env_var, str):
+                env_var = env_var.encode('utf-8')
+            if isinstance(value, str):
+                value = value.encode('utf-8')
+            env[env_var] = value
+    jt.jobEnvironment = env
 
     # Run module using python -m to avoid ImportErrors when unpickling jobs
     jt.remoteCommand =  sys.executable
