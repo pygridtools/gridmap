5cc72ba0fdc5ee8cb17a6d1eb16fbe307ca901a1
Dan Blanchard <dblanchard@ets.org>
2013-09-16 15:22:03 -0400

    Made tests more verbose.

--- tests/test_gridmap.py
+++ tests/test_gridmap.py
@@ -37,7 +37,7 @@ def compute_factorial(n):
 def test_map():
     inputs = [1, 2, 4, 8, 16]
     expected = map(compute_factorial, inputs)
-    outputs = grid_map(compute_factorial, inputs)
+    outputs = grid_map(compute_factorial, inputs, quiet=False)
     eq_(expected, outputs)
 
 
@@ -59,5 +59,5 @@ def test_process_jobs():
     inputs = [1, 2, 4, 8, 16]
     expected = map(compute_factorial, inputs)
     function_jobs = make_jobs(inputs, compute_factorial)
-    outputs = process_jobs(function_jobs)
+    outputs = process_jobs(function_jobs, quiet=False)
     eq_(expected, outputs)
