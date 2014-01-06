64208c5b0af308a2eef94690088bb89f1cb1145a
Dan Blanchard <dblanchard@ets.org>
2014-01-06 11:39:23 -0500

    Wrap JOB_IDS_SESSION_ALL in a list, because it needs to be that way

--- gridmap/job.py
+++ gridmap/job.py
@@ -405,7 +405,7 @@ class JobMonitor(object):
 
             # Get rid of job info to prevent memory leak
             with Session(self.session_id) as session:
-                session.synchronize(JOB_IDS_SESSION_ALL, TIMEOUT_NO_WAIT,
+                session.synchronize([JOB_IDS_SESSION_ALL], TIMEOUT_NO_WAIT,
                                     dispose=True)
 
     def check_if_alive(self):
