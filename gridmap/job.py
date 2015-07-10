ad9f5d61590c5ed3d9450219400d8c521e074dcf
Andreas Sorge <as@asorge.de>
2015-07-10 20:44:14 +0200

    Add option to (not) copy environment

--- gridmap/job.py
+++ gridmap/job.py
@@ -108,11 +108,12 @@ class Job(object):
                  'name', 'queue', 'environment', 'working_dir',
                  'cause_of_death', 'num_resubmits', 'home_address',
                  'log_stderr_fn', 'log_stdout_fn', 'timestamp', 'host_name',
-                 'heart_beat', 'track_mem', 'track_cpu', 'interpreting_shell')
+                 'heart_beat', 'track_mem', 'track_cpu', 'interpreting_shell',
+                 'copy_env')
 
     def __init__(self, f, args, kwlist=None, cleanup=True, mem_free="1G",
                  name='gridmap_job', num_slots=1, queue=DEFAULT_QUEUE,
-                 interpreting_shell=None):
+                 interpreting_shell=None, copy_env=True):
         """
         Initializes a new Job.
 
@@ -135,6 +136,8 @@ class Job(object):
         :type queue: str
         :param interpreting_shell: The interpreting shell for the job
         :type interpreting_shell: str
+        :param copy_env: copy environment from master node to worker node?
+        :type copy_env: boolean
         """
         self.track_mem = []
         self.track_cpu = []
@@ -161,19 +164,21 @@ class Job(object):
         self.name = name.replace(' ', '_')
         self.queue = queue
         self.interpreting_shell = interpreting_shell
+        self.copy_env = copy_env
         # Save copy of environment variables
         self.environment = {}
-        for env_var, value in os.environ.items():
-            try:
-                if not isinstance(env_var, bytes):
-                    env_var = env_var.encode()
-                if not isinstance(value, bytes):
-                    value = value.encode()
-            except UnicodeEncodeError:
-                logger = logging.getLogger(__name__)
-                logger.warning('Skipping non-ASCII environment variable.')
-            else:
-                self.environment[env_var] = value
+        if self.copy_env:
+            for env_var, value in os.environ.items():
+                try:
+                    if not isinstance(env_var, bytes):
+                        env_var = env_var.encode()
+                    if not isinstance(value, bytes):
+                        value = value.encode()
+                except UnicodeEncodeError:
+                    logger = logging.getLogger(__name__)
+                    logger.warning('Skipping non-ASCII environment variable.')
+                else:
+                    self.environment[env_var] = value
         self.working_dir = os.getcwd()
 
     @property
@@ -878,7 +883,7 @@ def _resubmit(session_id, job, temp_dir):
 def grid_map(f, args_list, cleanup=True, mem_free="1G", name='gridmap_job',
              num_slots=1, temp_dir='/scratch/', white_list=None,
              queue=DEFAULT_QUEUE, quiet=True, local=False, max_processes=1,
-             interpreting_shell=None):
+             interpreting_shell=None, copy_env=True):
     """
     Maps a function onto the cluster.
 
@@ -921,6 +926,8 @@ def grid_map(f, args_list, cleanup=True, mem_free="1G", name='gridmap_job',
     :type max_processes: int
     :param interpreting_shell: The interpreting shell for the jobs.
     :type interpreting_shell: str
+    :param copy_env: copy environment from master node to worker node?
+    :type copy_env: boolean
 
     :returns: List of Job results
     """
@@ -929,7 +936,8 @@ def grid_map(f, args_list, cleanup=True, mem_free="1G", name='gridmap_job',
     jobs = [Job(f, [args] if not isinstance(args, list) else args,
                 cleanup=cleanup, mem_free=mem_free,
                 name='{}{}'.format(name, job_num), num_slots=num_slots,
-                queue=queue, interpreting_shell=interpreting_shell)
+                queue=queue, interpreting_shell=interpreting_shell,
+                copy_env=copy_env)
             for job_num, args in enumerate(args_list)]
 
     # process jobs
